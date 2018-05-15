afa2b3e92630fe84f6da1debdba9c7bbcb811796
weiiipf <1098419929@qq.com>
2018-05-15 22:48:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var oLi = Array.prototype.slice.call(document.getElementsByTagName('li'));
+oLi.forEach(function (ele, index) {
+    ele.spec = getSpec(ele);
+    ele.addEventListener('mouseenter',function(e){
+        addClass(ele,e,'in')
+    })
+    ele.addEventListener('mouseleave',function(e){
+        addClass(ele,e,'out')
+    })
+})
+function getSpec(ele) {
+    return {
+        w: ele.offsetWidth,
+        h: ele.offsetHeight
+    }
+}
+function getDirection(ele,e){
+    var x = e.offsetX - ele.spec.w/2;
+    var y = e.offsetY - ele.spec.h/2;
+    return d = (Math.round(((Math.atan2(y,x) * (180/Math.PI)) + 180)/90) + 3)%4;
+}
+function addClass(ele,e,state){
+    getDirection(ele,e);
+    var direction;
+    switch(d){
+        case 0:
+        direction = '-top';
+        break;
+        case 1:
+        direction = '-right';
+        break;
+        case 2:
+        direction = '-bottom';
+        break;
+        case 3:
+        direction = '-left';
+        break;
+
+    }
+    ele.className = state + direction;
+}
